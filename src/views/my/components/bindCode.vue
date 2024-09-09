38cd9229eb57363f32af67471f5ab990a720f8b9
zhumama <zhumama@gmail.com>
2024-09-09 22:19:28 +0800

    个人详情完成

--- src/views/my/components/bindCode.vue
+++ src/views/my/components/bindCode.vue
@@ -2,9 +2,9 @@
   <div class="px-[16px] pt-[24px]">
     <div class="common-linear py-[18px] px-[20px] relative">
       <img
-        src="@/assets/friend/gift.svg"
-        alt=""
-        class="w-[48px] h-[48px] absolute left-[50%] top-0 translate-x-[-50%] translate-y-[-50%] z-[2002]"
+          src="@/assets/friend/gift.svg"
+          alt=""
+          class="w-[48px] h-[48px] absolute left-[50%] top-0 translate-x-[-50%] translate-y-[-50%] z-[2002]"
       />
       <div class="text-[20px] text-center my-[16px]">绑定获得新⼿⼤礼包</div>
       <div class="mb-2.5">
@@ -13,21 +13,21 @@
       </div>
       <div class="mb-[24px]">
         <code-input
-          ref="codeInput"
-          inputType="text"
-          width="100%"
-          :maxlength="5"
-          :focus="true"
+            ref="codeInput"
+            inputType="text"
+            width="100%"
+            :maxlength="5"
+            :focus="true"
         ></code-input>
       </div>
       <div class="my-button text-center bottom-[50px] w-[100%]">
         <van-button
-          class="shadow-btn-primary w-[50%]"
-          type="primary"
-          @click="handleBinding"
+            class="shadow-btn-primary w-[50%]"
+            type="primary"
+            @click="handleBinding"
         >
           <div class="flex items-center gap-[8px]">
-            <img src="@/assets/my/bind.png" alt="" />
+            <img src="@/assets/my/bind.png" alt=""/>
             <span>绑定</span>
           </div>
         </van-button>
@@ -35,19 +35,22 @@
     </div>
     <div class="text-center pt-[30px]">
       <img
-        src="@/assets/svg/close.svg"
-        class="inline-block w-[28px]"
-        @click="emit('close')"
+          src="@/assets/svg/close.svg"
+          class="inline-block w-[28px]"
+          @click="emit('close')"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import {ref, reactive} from 'vue'
 import CodeInput from '@/views/my/components/code-input.vue'
-import { showToast } from 'vant'
-import { bindInviteCode } from '@/services/user'
+import {showToast} from 'vant'
+import {bindInviteCode} from '@/services/user'
+import useStore from "@/store";
+
+const {accountStore} = useStore()
 
 const codeInput = ref(null)
 
@@ -58,8 +61,8 @@ const handleBinding = async () => {
   }
   const res = await bindInviteCode(code)
   if (res.code == 200) {
+    await accountStore.changeUserInfo()
     emit('close')
-    emit('updateUserInfo')
     return showToast('绑定成功')
   } else {
     return showToast(res.message)
