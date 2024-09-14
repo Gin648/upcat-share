3d73b4efcf65c4a16027ab81ccc1bb01e7c4381a
Klaus <bizklaus85@gmail.com>
2024-09-14 23:59:54 +0800

    生产配置

--- src/config/env.prod.ts
+++ src/config/env.prod.ts
@@ -5,8 +5,8 @@
 export default {
   telegramInveteUrl: 'https://t.me/mytestIboBot/iboapp',
   // telegramInveteUrl: 'https://t.me/mytestIboBot/iboapp',
-  env: 'development',
-  baseUrl: 'https://k5lkrpnzc0.execute-api.ap-southeast-1.amazonaws.com',
+  env: 'production',
+  baseUrl: ' https://av8ajwcbmk.execute-api.ap-southeast-1.amazonaws.com',
   // baseUrl: 'http://16.163.120.57:9981',
   imgUrl: '',
   // 时间周期
