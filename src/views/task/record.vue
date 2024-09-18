606eb70ba6db483ffa7f3b9abc14ce0b0b607c66
Gin648 <89142935+lin1126@users.noreply.github.com>
2024-09-18 21:37:19 +0800

    任务模块

--- src/views/task/record.vue
+++ src/views/task/record.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="pb-[20px]">
-    <NavBar title="任务记录"></NavBar>
+    <NavBar :title="t('ren-wu-ji-lu')"></NavBar>
     <div class="px-[16px]">
       <div class="flex gap-[11px]">
         <div class="flex-1">
           <ShadowBorderBox>
             <div class="flex h-[80px] flex-col items-center justify-center">
-              <div class="big_number text-[28px] leading-[28px]">10M</div>
-              <div class="text-[12px] mt-[9px]">累计获得IBO</div>
+              <div class="big_number text-[28px] leading-[28px]">
+                {{ formatNumberUnit(taskInfo.ratAmount) }}
+              </div>
+              <div class="text-[12px] mt-[9px] text-center">
+                {{ t('lei-ji-huo-de-jin-bi') }}
+              </div>
             </div>
           </ShadowBorderBox>
         </div>
         <div class="flex-1">
           <ShadowBorderBox>
             <div class="flex h-[80px] flex-col items-center justify-center">
-              <div class="big_number text-[28px] leading-[28px]">10000</div>
-              <div class="text-[12px] mt-[9px]">累计获得学习币</div>
+              <div class="big_number text-[28px] leading-[28px]">
+                {{ formatNumberUnit(taskInfo.catAmount) }}
+              </div>
+              <div class="text-[12px] mt-[9px] text-center">
+                {{ t('lei-ji-huo-de-zhuan-shi') }}
+              </div>
             </div>
           </ShadowBorderBox>
         </div>
@@ -24,31 +32,44 @@
       <VanList
         v-model:loading="state.loading"
         :finished="state.finished"
-        finished-text="没有更多了"
-        loading-text="加载中"
+        :finished-text="t('mei-you-geng-duo-le')"
+        :loading-text="t('jia-zai-zhong')"
         @load="onLoad"
       >
         <div
-          v-for="item in 10"
+          v-for="item in state.list"
           :key="item"
           :order="item"
           class="mt-[12px] common-linear py-[16px] px-[12px] flex items-center"
         >
-          <div
-            class="w-[56px] mr-[12px] shrink-0 h-[56px] bg-[#A8A8A8] rounded-[8px]"
-          ></div>
+          <img
+            :src="item.icon"
+            class="w-[56px] mr-[12px] shrink-0 h-[56px] rounded-[8px]"
+          />
           <div>
-            <div class="text-[14px]">点击打开TOM UP的推特主页</div>
+            <div class="text-[14px]">{{ item.name }}</div>
             <div class="text-[12px] opacity-60 mt-[8px]">
-              12:22:22 22/08/2024
+              {{ item.createTime }}
             </div>
           </div>
           <div
             class="flex flex-col mr-[12px] items-center justify-center ml-auto"
           >
             <img class="w-[31px]" src="@/assets/png/Rat_Coin.png" />
+            <div
+              v-if="item.ratAmount"
+              class="text-[20px] text-[#E0B374] mt-[4px] font-semibold"
+            >
+              +{{ formatNumberUnit(item.ratAmount) }}
+            </div>
+          </div>
+          <div
+            v-if="item.catAmount"
+            class="flex flex-col mr-[12px] items-center justify-center ml-auto"
+          >
+            <img class="w-[31px]" src="@/assets/png/Cat_Coin.png" />
             <div class="text-[20px] text-[#E0B374] mt-[4px] font-semibold">
-              +100
+              +{{ formatNumberUnit(item.catAmount) }}
             </div>
           </div>
         </div>
@@ -58,9 +79,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
 import NavBar from '@/components/NavBar/index.vue'
 import ShadowBorderBox from '@/components/ShadowBorderBox/index.vue'
+import { taskTatistics, userTaskListPage } from '@/services/task'
+import { formatNumberUnit } from '@/utils/utils'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
+const taskInfo = ref<any>({})
+
+const _taskTatistics = async () => {
+  const { success, data } = await taskTatistics()
+  if (success) {
+    taskInfo.value = data
+  }
+}
+
 const state = reactive({
   active: 0,
   page: 0,
@@ -80,12 +115,44 @@ const init = () => {
   onLoad()
 }
 
-const onLoad = async () => {
-  state.loading = true
-  state.page += 1
-  // await getRecordList()
+const getList = async () => {
+  const resp: any = await userTaskListPage({
+    page: state.page,
+    size: state.size,
+  })
+  if (!resp.success) {
+    state.finished = true
+    return
+  }
+
+  if (!resp.data || !+resp.data.total) {
+    state.finished = true
+    return
+  }
   state.loading = false
+  state.total = resp.data.total
+  state.list =
+    state.page === 1 ? resp.data.list : state.list.concat(resp.data.list)
+  // 数据全部加载完成
+  if (state.list.length >= state.total) {
+    state.finished = true
+  }
 }
+
+const onLoad = async () => {
+  try {
+    state.loading = true
+    state.page += 1
+    await getList()
+  } catch (error) {
+    state.finished = true
+  } finally {
+    state.loading = false
+  }
+}
+onMounted(() => {
+  _taskTatistics()
+})
 </script>
 
 <style scoped></style>
