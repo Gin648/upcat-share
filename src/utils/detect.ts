4ca575764645a861b78a9c3dd5a23295a803d0cb
Gin648 <89142935+lin1126@users.noreply.github.com>
2024-09-22 16:14:26 +0800

    观看广告

--- src/utils/detect.ts
+++ src/utils/detect.ts
@@ -3,15 +3,27 @@ export function detect(navigator) {
   let environment = ''
   if (window.Telegram?.WebApp.initData) {
     environment = 'tg'
-  } else if (webview) {
-    var ua = navigator.userAgent
-    var platform = navigator.platform
-    var chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/)
-    var webview =
-      !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/)
-    environment = 'webview'
   } else {
-    environment = 'browser'
+    var ua = navigator.userAgent
+    const chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/)
+    const webview =
+      !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/)
+    if (webview) {
+      environment = 'webview'
+    } else {
+      environment = 'browser'
+    }
   }
+
+  // else if (webview) {
+  //   var ua = navigator.userAgent
+  //   var platform = navigator.platform
+  //   var chrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/)
+  //   var webview =
+  //     !chrome && ua.match(/(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/)
+  //
+  // } else {
+  //   environment = 'browser'
+  // }
   return environment
 }
